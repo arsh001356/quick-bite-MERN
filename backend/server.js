841cc74c60182b1577abca7d7c8599c524082e23
arsh001356 <arsh001356@gmail.com>
2025-02-08 20:04:22 +0530

    4 commit

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,40 @@
-import express from "express"
-import dotenv from "dotenv"
-import mongoose from 'mongoose';
-import cors from 'cors';
-import userRoute from './routes/user.route.js'
-import BookTableRoute from "./routes/bookTable.route.js"
-import BookingRoute from "./routes/booking.route.js"
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import userRoute from "./routes/user.route.js";
+import BookTableRoute from "./routes/bookTable.route.js";
+import BookingRoute from "./routes/booking.route.js";
 
-
+dotenv.config();
 
 const app = express();
-app.use(express.json())
-app.use(cors())
-dotenv.config()
-
 const port = process.env.PORT || 3000;
-const mongoDbUri = process.env.MONGO_DB_URI
+const mongoDbUri = process.env.MONGO_DB_URI;
+const frontendUrl = process.env.FRONTEND_URL;
 
-main().then(() => { console.log("DB connected") }).catch(err => console.log(err));
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: frontendUrl,
+        credentials: true,
+    })
+);
 
 async function main() {
     await mongoose.connect(mongoDbUri);
-
+    console.log("DB connected");
 }
+main().catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('api working');
+app.get("/", (req, res) => {
+    res.send("API working");
 });
-app.use("/user", userRoute)
-app.use(BookTableRoute)
-app.use(BookingRoute)
 
+app.use("/user", userRoute);
+app.use(BookTableRoute);
+app.use(BookingRoute);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
