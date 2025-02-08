280766e75729bdd484de11b4b9023af54c56d2dd
arsh001356 <arsh001356@gmail.com>
2025-02-08 18:33:29 +0530

    3 commit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ import BookingRoute from "./routes/booking.route.js"
 
 const app = express();
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 dotenv.config()
 
 const port = process.env.PORT || 3000;
